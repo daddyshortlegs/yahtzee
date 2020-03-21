585079804a1c6f3033a6d210de09ef429657b1ba
Andy Smith <andysmith836@gmail.com>
2020-03-21 22:44:03 +0000

    Got one case working

--- yahtzee_test.py
+++ yahtzee_test.py
@@ -2,13 +2,14 @@ import unittest
 
 
 def ones(rolls):
-    return 2
+    ones = filter(lambda roll: roll == 1, rolls)
+    return sum(ones)
 
 
 class YahtzeeTestCase(unittest.TestCase):
     def test_ones(self):
-        score = ones([1, 1, 2, 3, 4])
-        self.assertEqual(2, score)
+        self.assertEqual(2, ones([1, 1, 2, 3, 4]))
+        self.assertEqual(3, ones([1, 1, 1, 4, 5]))
 
 
 if __name__ == '__main__':
