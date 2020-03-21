095dab84cc9dcd9f49d477644d18e6b9f35f493c
Andy Smith <andysmith836@gmail.com>
2020-03-21 22:45:08 +0000

    twos working

--- yahtzee_test.py
+++ yahtzee_test.py
@@ -2,8 +2,11 @@ import unittest
 
 
 def ones(rolls):
-    ones = filter(lambda roll: roll == 1, rolls)
-    return sum(ones)
+    return sum(filter(lambda roll: roll == 1, rolls))
+
+
+def twos(rolls):
+    return sum(filter(lambda roll: roll == 2, rolls))
 
 
 class YahtzeeTestCase(unittest.TestCase):
@@ -11,6 +14,9 @@ class YahtzeeTestCase(unittest.TestCase):
         self.assertEqual(2, ones([1, 1, 2, 3, 4]))
         self.assertEqual(3, ones([1, 1, 1, 4, 5]))
 
+    def test_twos(self):
+        self.assertEqual(4, twos([1, 1, 2, 2, 4]))
+        self.assertEqual(6, twos([2, 1, 2, 2, 4]))
 
 if __name__ == '__main__':
     unittest.main()
